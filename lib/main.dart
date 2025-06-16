b497ec2ae1f6ee7eee80541e02813affd4c77929
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-15 23:27:12 -0500

    correct var falsex for false

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter BLoC',
-      debugShowCheckedModeBanner: falsex,
+      debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
       theme: AppTheme(isDarkmode: false).getTheme(),
     );
